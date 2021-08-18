f8fefc44c93b3e30116480b489166dc6594955bc
herry88 <herry081288@gmail.com>
2021-08-18 13:40:38 +0700

    belajarRute

--- kkgsi_tokoonline/routes/web.php
+++ kkgsi_tokoonline/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//
+Route::get('belajar', function () {
+    return '<h2>Ini berasalh dari RouteBaru</h2>';
+});
