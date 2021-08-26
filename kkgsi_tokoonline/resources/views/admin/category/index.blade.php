036d8163e95abefaa8d8f07e409c8cfccef6a312
herry88 <herry081288@gmail.com>
2021-08-26 09:17:24 +0700

    Ui indexCategory

--- kkgsi_tokoonline/resources/views/admin/category/index.blade.php
+++ kkgsi_tokoonline/resources/views/admin/category/index.blade.php
@@ -5,11 +5,43 @@
 @endsection
 
 @section('content-title')
-<div class="d-flex align-items-center">
-    <h5 class="font-medium m-b-0">Category</h5>
-    <div class="custom-breadcrumb ml-auto">
-        <a href="#!" class="breadcrumb">Home</a>
-        <a href="#!" class="breadcrumb">Dashboard</a>
+    <div class="d-flex align-items-center">
+        <h5 class="font-medium m-b-0">Category</h5>
+        <div class="custom-breadcrumb ml-auto">
+            <a href="#!" class="breadcrumb">Home</a>
+            <a href="#!" class="breadcrumb">Dashboard</a>
+        </div>
     </div>
-</div>
+@endsection
+@section('content')
+    <div class="row">
+        <div class="col s12">
+            <div class="card">
+                <div class="card-content">
+                    <h5 class="card-title">Data Category</h5>
+                    <p class="caption"><a class="waves-effect waves-light btn btn-round indigo">Add Data</a></p>
+                    <div class="row">
+                        <div class="col s12">
+                            <table class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>tes</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
