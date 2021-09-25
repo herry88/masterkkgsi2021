6547355b7c5e3f71413d7023b240ce2b34cebe88
herry88 <herry081288@gmail.com>
2021-09-25 13:51:44 +0700

    pondasi

---
+++ kkgsiproject/lib/api/api_utils.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+enum RequestType{Post, Get, PostWithAuth, GetWitAuth}
+
+class ApiUtil{
+  //config
+  static const String IP_ADDRESS = "skielstore.herryprasetyo.my.id";
+  static const String BASE_URL = "https://" + IP_ADDRESS; 
+
+  //response 200, 404, 500
+
+  //header
+}
--- kkgsiproject/lib/main.dart
+++ kkgsiproject/lib/main.dart
@@ -40,6 +40,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Login(),
+    );
   }
 }
