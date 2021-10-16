0c0e36740e8b17746af695804a608de60dc8b446
herry88 <herry081288@gmail.com>
2021-10-16 23:38:42 +0700

    mainDart

--- kkgsiproject/lib/api/api_utils.dart
+++ kkgsiproject/lib/api/api_utils.dart
@@ -1,18 +1,63 @@
 import 'package:flutter/material.dart';
 
-enum RequestType{Post, Get, PostWithAuth, GetWitAuth}
+enum RequestType { Post, Get, PostWithAuth, GetWithAuth }
 
-class ApiUtil{
+class ApiUtil {
   //config
   static const String IP_ADDRESS = "skielstore.herryprasetyo.my.id";
   static const String DefaultPage = "rest/bootstrap";
-  static const String BASE_URL = "https://" + IP_ADDRESS + "/" + DefaultPage; 
+  static const String BASE_URL = "https://" + IP_ADDRESS + "/" + DefaultPage;
 
   static const String RegisterPage = "rest/register";
 
   static const String CategoryProduct = "/rest/categoryProducts";
 
   //response 200, 404, 500
+  static const int SUCCESS_CODE = 200;
+  static const int ERROR_CODE = 400;
+  static const int UNAUTHORIZED_CODE = 401;
 
-  //header
+  //Custom codes
+  static const int INTERNET_NOT_AVAILABLE_CODE = 500;
+  static const int SERVER_ERROR_CODE = 501;
+  static const int MAINTENANCE_CODE = 503;
+
+  //------------------ Header ------------------------------//
+
+  static Map<String, String> getHeader(
+      {RequestType requestType = RequestType.Get, String token = ""}) {
+    switch (requestType) {
+      case RequestType.Post:
+        return {
+          "Accept": "application/json",
+          "Content-type": "application/json"
+        };
+      case RequestType.Get:
+        return {
+          "Accept": "application/json",
+        };
+      case RequestType.PostWithAuth:
+        return {
+          "Accept": "application/json",
+          "Content-type": "application/json",
+          "Authorization": "Bearer " + token
+        };
+      case RequestType.GetWithAuth:
+        return {
+          "Accept": "application/json",
+          "Authorization": "Bearer " + token
+        };
+    }
+    //Not reachable
+    return {"Accept": "application/json"};
+  }
+
+  //body
+  static Map<String, dynamic> getPatchRequestBody() {
+    return {'_method': 'PATCH'};
+  }
+  //app data 
+  static const String APP_DATA = 'rest/bootstrap/';
+  //user profile
+  static const String USER = 'rest/accountInfo/';
 }
