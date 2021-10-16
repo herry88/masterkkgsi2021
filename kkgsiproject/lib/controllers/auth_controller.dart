4ca9568411dee6a3afeb48329804b8632c2090b1
herry88 <herry081288@gmail.com>
2021-10-16 23:44:09 +0700

    addResponse

---
+++ kkgsiproject/lib/controllers/auth_controller.dart
@@ -0,0 +1,8 @@
+import 'package:kkgsiproject/models/myresponse.dart';
+
+class AuthController{
+  //login
+  static Future<MyResponse> loginUser(String email, String password) async{
+    
+  }
+}
--- kkgsiproject/lib/main.dart
+++ kkgsiproject/lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key key}) : super(key: key);
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
