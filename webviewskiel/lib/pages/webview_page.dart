433476390d53b64006fc1afd68fe0f81bb945649
herry88 <herry081288@gmail.com>
2021-10-25 08:59:52 +0700

    ya udah

---
+++ webviewskiel/lib/pages/webview_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:webviewskiel/pages/splashscreen.dart';
+
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({ Key key }) : super(key: key);
+
+  @override
+  _WebViewPageState createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      
+    );
+  }
+}
